0263e8e7a441156ee0f6dbd66130fcd972520001
Yehor Boichenko <boichenko.yehor@gmail.com>
2022-12-14 14:13:45 +0200

    Update Banner.jsx

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -12,6 +12,7 @@ export const Banner = () => {
   const [isDeleting, setIsDeleting] = useState(false);
   const [text, setText] = useState("");
   const [delta, setDelta] = useState(300 - Math.random() * 100);
+  // eslint-disable-next-line
   const [index, setIndex] = useState(1);
   const toRotate = [
     "Front-end Developer",
@@ -28,6 +29,7 @@ export const Banner = () => {
     return () => {
       clearInterval(ticker);
     };
+    // eslint-disable-next-line
   }, [text]);
 
   const tick = () => {
